e95e6235382342d34996cd218b05db61b00ccade
Sudip Sarker <sudipsarker2016@gmail.com>
2025-05-09 19:36:04 +0600

    [2666] [Time Beats: 57.86%] [Memory Beats: 86.59%] - LeetPush

---
+++ Allow-One-Function-Call.js
@@ -0,0 +1,14 @@
+function once(fn) {
+    let called = false;
+    let result;
+
+    return function(...args) {
+        if (!called) {
+            called = true;
+            result = fn(...args);
+            return result;
+        }
+        return undefined;
+    };
+}
+
